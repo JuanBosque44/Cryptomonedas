7bcf56212ea7c89516c01e08c7875b4a72dba2c0
Juan <nachobosque09@gmail.com>
2025-05-27 11:06:49 -0300

    Agregar archivos de proyecto.

---
+++ Backend/ApiCriptos/Controllers/CryptoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCriptos.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class CryptoController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
